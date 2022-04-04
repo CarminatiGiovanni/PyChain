e2e6c43cadd2cccaa916efcc8dfec7d18506674d
HeyJOe <jocarmi03@gmail.com>
2022-04-04 19:17:33 +0200

    modified fodler name

--- tests/io/__init__.py
+++
@@ -1,4 +0,0 @@
-from clientio_1 import clientio1
-from clientio_2 import clientio2
-from serverio_1 import serverio1
-from serverio_2 import serverio2
--- tests/server_flask_socketio_client_2.py
+++ tests/server_flask_socketio_client_2.py
@@ -1,22 +1,23 @@
 from flask import Flask
-from io import clientio2, serverio2
+from server_client_io import clientio2, serverio2
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def main():
-    return "Hello world",200
+    return "Hello world", 200
+
 
 @app.route('/connect')
-def connet_client():
+def connect_client():
     try:
         clientio2.connect('http://localhost:4000/')
         print('client connected')
-        return "Client connected",200
+        return "Client connected", 200
     except Exception as e:
         return f"Client not connected: {str(e)}", 500
 
 
-
 if __name__ == '__main__':
-    serverio2.run(app, port= 5000, debug=True)
+    serverio2.run(app, port=5000, debug=True)
