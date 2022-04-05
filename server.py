292f72fb0f912001d6da08873a4b0cb026bf6197
HeyJOe <jocarmi03@gmail.com>
2022-04-05 12:47:48 +0200

    moved files

--- server.py
+++ server.py
@@ -1,20 +1,6 @@
-from flask import Flask, jsonify
-from config import PORT, HOST
-from globals import b
-from routes import *
+from app import create_app,serverio
 
-# instantiate the app component, also set static and HTML templates folder
-app = Flask(__name__, template_folder="templates", static_folder="static")
-
-
-@app.route('/',methods=['POST','GET'])
-def entry_page():
-    return jsonify(blockchain = b.to_dict())
+app = create_app(debug=True)
 
 if __name__ == '__main__':
-    try:
-        # THIS STARTS THE SERVER
-        # this is a BLOCKING TASK !!!
-        app.run(debug=True, host=HOST, port=PORT)
-    except Exception as e:
-        print(str(e))
+    serverio.run(app, port=5000)
