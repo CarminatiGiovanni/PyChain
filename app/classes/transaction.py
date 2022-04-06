eb1ae365db9dd9441444d36256f0176a05bd4280
HeyJOe <jocarmi03@gmail.com>
2022-04-06 18:53:22 +0200

    defined new transactions

--- app/classes/transaction.py
+++ app/classes/transaction.py
@@ -1,21 +1,23 @@
 class Transaction:
 
-    def __init__(self, sender: str, receiver: str, value: int, timestamp: float):
-        self.sender = sender
-        self.receiver = receiver
+    def __init__(self, content_type: str, author: str, value: str, description: str, timestamp: float):
+        self.content_type = content_type
+        self.author = author
         self.value = value
+        self.description = description
         self.timestamp = timestamp
 
     @classmethod
     def from_dict(cls, transaction: dict):
-        sender = transaction['sender']
-        receiver = transaction['receiver']
+        content_type = transaction['content_type']
+        author = transaction['author']
         value = transaction['value']
+        description = transaction['description']
         timestamp = transaction['timestamp']
-        return Transaction(sender,receiver,value,timestamp)
+        return Transaction(content_type, author, value, description, timestamp)
 
     def __str__(self) -> str:
-        return "{" + f"sender:{self.sender},receiver:{self.receiver},value:{self.value},timestamp:{self.timestamp}" + "}"
+        return "{" + f"content_type:{self.content_type},author:{self.author},value:{self.value},description:{self.description},timestamp:{self.timestamp}" + "}"
 
     def to_dict(self):
-        return {'sender': self.sender, 'receiver': self.receiver, 'value': self.value, "timestamp": self.timestamp}
+        return {'content_type': self.content_type, 'author': self.author, 'value': self.value, "description": {self.description}, "timestamp": self.timestamp}
