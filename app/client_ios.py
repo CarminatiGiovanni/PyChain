f75513613b957b3f49a7668274e885c82a170b13
HeyJOe <jocarmi03@gmail.com>
2022-05-05 10:51:24 +0200

    added a print

--- app/client_ios.py
+++ app/client_ios.py
@@ -27,6 +27,7 @@ class ClientIOS:
                 print("impossible connect to " + node + '\n' + str(e))
 
     def emit(self, call, message):
+        print(str(message))
         if message is None:
             return
         for c in self.ios:
