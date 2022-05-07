fd0111f56dbc2a131697ccfb04f125ca9e57ee2d
HeyJOe <jocarmi03@gmail.com>
2022-05-07 14:19:29 +0200

    created app form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,24 @@
-function App() {
-  return (
-    <div className="App">Hello world </div>
-  );
+import React, { MouseEventHandler } from "react";
+
+
+class App extends React.Component {
+
+  handleSubmit = (e: React.MouseEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    console.log((e.currentTarget.elements.namedItem("server address") as HTMLInputElement).value)
+  }
+
+  render() {
+      return (
+        <div className="App">
+          <form onSubmit={this.handleSubmit}>
+            <input id="server_address" name="server address" placeholder="Server address:" type="text"/>
+            <input id="submit_button" type="submit" placeholder="Save"/>
+          </form>
+        </div>
+      );
+  }
 }
 
-export default App;
+export default App
