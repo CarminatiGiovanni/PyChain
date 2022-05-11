758ac7bd1337a6035fa02098128a43f28eb11eb4
HeyJOe <jocarmi03@gmail.com>
2022-05-11 10:39:36 +0200

    host defined

--- blockchain/server.py
+++ blockchain/server.py
@@ -1,10 +1,10 @@
 from app import create_app, serverio
-from app.config import PORT, template_folder, static_folder
+from app.config import PORT, template_folder, static_folder, HOST
 
 app = create_app(debug=True, template_folder=template_folder, static_folder=static_folder)
 
 if __name__ == '__main__':
     try:
-        serverio.run(app,host= "0.0.0.0", port=PORT)
+        serverio.run(app,host= HOST, port=PORT)
     except Exception as e:
         print(str(e))
