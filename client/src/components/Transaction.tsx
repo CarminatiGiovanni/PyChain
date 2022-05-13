7f2fdc55b8ed29781bb7edc1aa6872ab01010977
HeyJOe <jocarmi03@gmail.com>
2022-05-13 09:48:39 +0200

    added transaction

--- client/src/components/Transaction.tsx
+++ client/src/components/Transaction.tsx
@@ -1,10 +1,30 @@
 import { TransactionInterface } from "./../classes";
+import './../style/css/transaction_block.css'
 
 const Transaction = ({transaction} : {transaction: TransactionInterface}) => {
     return(
-        <>
-        transaction
-       </>
+        
+        <div className="container">
+            <div className="info__container">
+                <h2 className="title__container">laudi del cielo del mare della terra degli eroi</h2><button className="btn__container">SHOW</button>
+            </div>
+            <div className="info__container">
+                <h2 className="title__container">laudi del cielo del mare della terra degli eroi</h2><button className="btn__container">SHOW</button>
+            </div>
+            <div className="info__container">
+                <h2 className="title__container">laudi del cielo del mare della terra degli eroi</h2><button className="btn__container">SHOW</button>
+            </div>
+            <div className="info__container">
+                <h2 className="title__container">laudi del cielo del mare della terra degli eroi</h2><button className="btn__container">SHOW</button>
+            </div>
+            <div className="info__container">
+                <h2 className="title__container">laudi del cielo del mare della terra degli eroi</h2><button className="btn__container">SHOW</button>
+            </div>
+            <div className="info__container">
+                <h2 className="title__container">laudi del cielo del mare della terra degli eroi</h2><button className="btn__container">SHOW</button>
+            </div>
+        </div>
+       
     )
 }
 
